167da1c0bc04d511874da0ec0fb6294b1ab3ad36
FDweb0 <110075941+FDweb0@users.noreply.github.com>
2022-08-19 02:19:01 -0700

    Update Settings.java

--- app/src/main/java/com/full/gallery/top/secure/password/photos/Settings.java
+++ app/src/main/java/com/full/gallery/top/secure/password/photos/Settings.java
@@ -1,36 +1,11 @@
 package com.full.gallery.top.secure.password.photos;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import com.android.billingclient.BuildConfig;
-import com.android.billingclient.api.BillingClient;
-import com.android.billingclient.api.BillingClientStateListener;
-import com.android.billingclient.api.BillingFlowParams;
-import com.android.billingclient.api.BillingResult;
-import com.android.billingclient.api.ProductDetails;
-import com.android.billingclient.api.ProductDetailsResponseListener;
-import com.android.billingclient.api.Purchase;
-import com.android.billingclient.api.PurchasesUpdatedListener;
-import com.android.billingclient.api.QueryProductDetailsParams;
-import com.android.billingclient.api.SkuDetails;
-import com.fulldive.startapppopups.PopupManager;
-import com.fulldive.startapppopups.donation.DonationAction;
 import com.fulldive.startapppopups.donation.DonationManager;
-import com.fulldive.startapppopups.donation.DonationSnackbar;
-import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.collect.ImmutableList;
-
-import java.util.List;
-
-import kotlin.Unit;
-import kotlin.jvm.functions.Function0;
+import com.fulldive.startapppopups.PopupManager;
 
 public class Settings extends AppCompatActivity {
 
