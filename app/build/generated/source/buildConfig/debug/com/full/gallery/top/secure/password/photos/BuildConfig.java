7473de29d8a929ca54c1accb1ed175bb04fc7e9c
FDweb0 <110075941+FDweb0@users.noreply.github.com>
2022-09-13 19:41:26 +0500

    Icon Changed
    Icon Changed

---
+++ app/build/generated/source/buildConfig/debug/com/full/gallery/top/secure/password/photos/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.full.gallery.top.secure.password.photos;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.full.gallery.top.secure.password.photos";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
